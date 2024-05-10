13374645155b300327725d87a7a8de0655a44523
Maximilian Eisenach <Maximilian.Eisenach@st.oth-regensburg.de>
2024-05-10 18:00:16 +0200

    Basic Functionality is da, colour/blackwhite mode und skip button sind drin, spielbar ist es jetzt. Nur das mit dem differences anklicken könnt man jetz noch gucken.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Variables //
 
-let imageIndex = 1;
-
-let timeoutId; // Variable zum Speichern der Timeout-ID
-let remainingTime = 60; // Startzeit für den Timer (in Sekunden)
-let timerRunning = false; // Flag, um den Timer-Status zu verfolgen
+let imageIndex = 0;
+const maxTime = 10; // in seconds
+let timeoutId;
+let remainingTime = maxTime; //in seconds
+let timerRunning = false;
 
 // Arrays with image filepaths //
 
@@ -29,53 +29,116 @@ hideElement(btnContinue);
 hideElement(timerElement);
 hideElement(btnRestart);
 hideElement(displayedImage);
+hideElement(gameOverElement);
+
 
 
 // Functions //
 
-function hideElement (element) {
+function hideElement (element)
+{
   element.style.visibility = "hidden";
 }
 
-function showElement (element) {
+function showElement (element)
+{
   element.style.visibility = "visible";
 }
 
-function runGame() {
 
+function checkTimerForImage()
+{
+    remainingTime--;
+    timerElement.textContent = remainingTime + " seconds";
+
+      if (remainingTime<=0)
+      {
+        clearInterval(timeoutId);
+        if (imageIndex >= 9)
+          {
+            displayGameOver();
+            return;
+          }
+          else 
+          {
+            imageIndex++;
+            displayedImage.src = imagesToUse[imageIndex];
+            remainingTime = maxTime;
+            timerElement.textContent = remainingTime + " seconds";
+            timeoutId = setInterval(checkTimerForImage, 1000);
+            return;
+          }
+        
+      }
+
+      else {
+        // Time hasn't expired -> just continue to next iteration of loop
+      }
 }
+
+function displayGameOver()
+{
+  console.log("initiating game over");
+  hideElement(displayedImage);
+  hideElement(timerElement);
+  hideElement(btnContinue);
+  showElement(gameOverElement);
+}
+
+function runGame()
+{
+  console.log("runGame entered");
+  timeoutId = setInterval(checkTimerForImage, 1000);
+}
+
+
+    
 
  // Button Click Events //
 console.log("buttons should work now");
 btnColours.addEventListener("click", () => {
-  imagesToUse = [...images]; // Use the original color image list
+  imagesToUse = images.slice(0, images.length); // Use the original color image list
+  displayedImage.src = imagesToUse[0];
   hideElement(btnColours);
   hideElement(btnBlackWhite);
   showElement(btnContinue);
   showElement(timerElement);
   showElement(displayedImage);
   console.log("colours pressed");
-  //runGame();
+  runGame();
 });
 
 btnBlackWhite.addEventListener("click", () => {
-  imagesToUse = [...images_bw]; // Use the black and white image list
+  imagesToUse = imagesToUse = images_bw.slice(0, images_bw.length);; // Use the black and white image list
+  displayedImage.src = imagesToUse[0];
   hideElement(btnColours);
   hideElement(btnBlackWhite);
   showElement(btnContinue);
   showElement(timerElement);
   showElement(displayedImage);
   console.log("BW pressed");
+  runGame();
 });
 
 btnContinue.addEventListener("click", () => {
-
+  clearInterval(timeoutId);
+        if (imageIndex >= 9)
+          {
+            displayGameOver();
+          }
+          else 
+          {
+            imageIndex++;
+            displayedImage.src = imagesToUse[imageIndex];
+            remainingTime = maxTime;
+            timerElement.textContent = remainingTime + " seconds";
+            timeoutId = setInterval(checkTimerForImage, 1000);
+          }
 });
 
 btnRestart.addEventListener("click", () => {
   
 });
-
 
 
 
